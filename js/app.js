d8cc7f96ee2e7c3052640b1e2830ef8892f61fbd
ayushjain <ayush.071998@gmail.com>
2021-04-20 16:57:33 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// window.onscroll = function () {
+//     myFunction()
+// };
+
+// var navbar = document.getElementById("navbar");
+// var sticky = 500;
+
+// function myFunction() {
+//     if (window.pageYOffset >= sticky) {
+//         navbar.classList.add("fixed-top")
+//     } else {
+//         navbar.classList.remove("fixed-top");
+//     }
+// }
